4cca2e968197785b269f50614737ccc8320e5bab
benocholy2 <benjamin.isidahomen@gloo.ng>
2020-02-21 09:55:29 +0100

    wip

---
+++ Gloo/OrderStatusSync/Cron/UpdateOrderStatusInCore.php
@@ -0,0 +1,75 @@
+<?php
+namespace Gloo\OrderStatusSync\Cron;
+
+use Zend\Json\Json;
+
+class UpdateOrderStatusInCore {
+
+    public $logger;
+    public $iterator;
+    public $orderFactory;
+    public $httpClient;
+
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Model\ResourceModel\Iterator $iterator,
+        \Gloo\OrderStatusSync\Model\OrderFactory $orderFactory,
+        \Zend\Http\Client $httpClient 
+    )
+    {
+        $this->logger = $logger;
+        $this->iterator = $iterator;
+        $this->orderFactory = $orderFactory;
+        $this->httpClient = $httpClient;
+    }
+
+    public function execute()
+    {
+        try {
+            $orderCollection = $this->orderFactory->create()->getCollection();
+            $this->iterator
+                ->walk(
+                    $orderCollection->getSelect(),
+                    [[$this, 'syncWithCore']]
+                );
+            $this->logger->info("All order available have been sync with core");
+    
+          } catch(\Exception $e){
+              $this->logger->critical('There was an error syncing with core, a retry will be attempted in the next sync ======> '. $e->getMessage());
+          }
+    
+    }
+
+    public function syncWithCore($order){
+        $order['row']['magento_core_secret'] = 'secret';
+        $params = $order['row'];
+        $incrementId = $order['row']['increment_id'];
+
+        try {
+            $this->httpClient->reset();
+            $this->httpClient->setUri('http://192.168.99.100:1337/sync');
+            $this->httpClient->setMethod('POST');
+            $this->httpClient->setHeaders([
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+               ]);
+            $this->httpClient->setRawBody(Json::encode($params));
+       	    $this->httpClient->send();
+            $response = $this->httpClient->getResponse();
+    
+            $statusCode = $response->getStatusCode();
+    
+            if($statusCode !== 201){
+                $this->logger->critical("Order with increment id {$incrementId} failed to sync with core, an attempt will be made in the next sync === $statusCode");
+            } else {
+                $orderFactory = $this->orderFactory->create();
+                $order = $orderFactory->load($incrementId, 'increment_id');
+                $order->delete();
+        
+                $this->logger->info("sync with core {$data}");
+            }
+        } catch (\Zend\Http\Exception\RuntimeException $runtimeException){
+            $this->logger->critical($runtimeException->getMessage());   
+        }
+    }
+}
